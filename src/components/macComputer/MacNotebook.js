a53a8bc2ca92b4493c3452d8d7ef0a39504a94e4
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-03-30 20:01:34 -0400

    finised up notebook section

--- src/components/macComputer/MacNotebook.js
+++ src/components/macComputer/MacNotebook.js
@@ -1,6 +1,27 @@
 import "../../styles/mac/MacNotebook.css";
+import NotebookSpecs from "./NotebookSpecs";
 
-const MacNotebook = ({ computerImage, colorOptionImage, price, macTitle }) => {
+const MacNotebook = ({
+  computerImage,
+  colorOptionImage,
+  price,
+  macTitle,
+  chipImage,
+  chipTitle,
+  cpuTitle,
+  gpuLevel,
+  gpuTitle,
+  ramMemoryDesc,
+  storageTitle,
+  retinaDisplayTitle,
+  retinaDisplayDesc,
+  batteryDesc,
+  facetimeCameraDesc,
+  weightTitle,
+  touchIDImage,
+  touchIdDesc,
+  className
+}) => {
   return (
     <div className="noteboook-container">
       <div className="notebook-top-section">
@@ -14,55 +35,65 @@ const MacNotebook = ({ computerImage, colorOptionImage, price, macTitle }) => {
       </div>
 
       <div className="notebook-bottom-section">
-        <div className="notebook-chip">
-          <img src="" alt="" />
-          <p>Apple M1 chip</p>
+        <div className={`notebook-chip notebook-items ${className}`}>
+          <img src={chipImage} alt="" />
+          <p>{chipTitle}</p>
         </div>
 
-        <div className="notebook-cpu">
-          <h3>8-core</h3>
+        <div className="notebook-cpu notebook-items">
+          <h3>{cpuTitle}</h3>
           <p>CPU</p>
         </div>
 
-        <div className="notebook-gpu">
-          <p>Up to</p>
-          <h3>8-core</h3>
+        <div className="notebook-gpu notebook-items">
+          <p>{gpuLevel}</p>
+          <h3>{gpuTitle}</h3>
           <p>GPU</p>
         </div>
 
-        <div className="notebook-ram-memory">
-          <img src="" alt="" />
-          <p>Up to 16GB </p>
+        <div className="notebook-ram-memory notebook-items">
+          <img
+            src="https://www.apple.com/v/mac/home/bl/images/overview/compare/compare_icon_unified_memory__emykppauav2a_large_2x.png"
+            alt=""
+          />
+          <p>{ramMemoryDesc} </p>
+          <p>unified memory</p>
         </div>
 
-        <div className="notebook-storage">
-          <h3>2TB</h3>
+        <div className="notebook-storage notebook-items">
+          <h3>{storageTitle}</h3>
           <p>Maximum configurable storage</p>
         </div>
 
-        <div className="notebook-retina-display">
-          <h3>13.3"</h3>
-          <p>Retina display</p>
+        <div className="notebook-retina-display notebook-items">
+          <h3>{retinaDisplayTitle}"</h3>
+          <p>{retinaDisplayDesc}</p>
         </div>
 
-        <div className="notebook-battery">
-          <img src="" alt="" />
-          <p>Up to 18 hours battery life</p>
+        <div className="notebook-battery notebook-items">
+          <img
+            src="https://www.apple.com/v/mac/home/bl/images/overview/compare/compare_icon_battery__fk0ughiyc3am_large_2x.png"
+            alt=""
+          />
+          <p>{batteryDesc}</p>
         </div>
 
-        <div className="notebook-facetime-camera">
-          <img src="" alt="" />
-          <p>720p Facetime HD camera</p>
+        <div className="notebook-facetime-camera notebook-items">
+          <img
+            src="https://www.apple.com/v/mac/home/bl/images/overview/compare/compare_icon_camera__dlxow9r3leie_large_2x.png"
+            alt=""
+          />
+          <p>{facetimeCameraDesc}</p>
         </div>
 
-        <div className="notebook-weight">
-          <h3>2.8lb.</h3>
+        <div className="notebook-weight notebook-items">
+          <h3>{weightTitle}</h3>
           <p>Weight</p>
         </div>
 
-        <div className="notebook-touch-id">
-          <img src="" alt="" />
-          <p>Touch ID</p>
+        <div className="notebook-touch-id notebook-items">
+          <img src={touchIDImage} alt="" />
+          <p>{touchIdDesc}</p>
         </div>
       </div>
     </div>
---
+++ src/components/macComputer/NotebookSpecs.js
@@ -0,0 +1,75 @@
+// import "../../styles/mac/NotebookSpecs.css";
+
+// const NotebookSpecs = ({
+//   chipImage,
+//   chipTitle,
+//   cpuTitle,
+//   gpuLevel,
+//   gpuTitle,
+//   reamMemoryDesc,
+//   storageTitle,
+//   retinaDisplayTitle,
+//   retinaDisplayDesc,
+//   batteryDesc,
+//   facetimeCameraDesc,
+//   weightTitle,
+//   touchIDImage,
+//   touchIdDesc
+// }) => {
+//   return (
+//     <div className="notebook-specs-container">
+//       <div className="notebook-chip">
+//         <img src="" alt="" />
+//         <p>{chipTitle}</p>
+//       </div>
+
+//       <div className="notebook-cpu">
+//         <h3>{cpuTitle}</h3>
+//         <p>CPU</p>
+//       </div>
+
+//       <div className="notebook-gpu">
+//         <p>{gpuLevel}</p>
+//         <h3>{gpuTitle}</h3>
+//         <p>GPU</p>
+//       </div>
+
+//       <div className="notebook-ram-memory">
+//         <img src="" alt="" />
+//         <p>{reamMemoryDesc} </p>
+//       </div>
+
+//       <div className="notebook-storage">
+//         <h3>{storageTitle}</h3>
+//         <p>Maximum configurable storage</p>
+//       </div>
+
+//       <div className="notebook-retina-display">
+//         <h3>{retinaDisplayTitle}"</h3>
+//         <p>{retinaDisplayDesc}</p>
+//       </div>
+
+//       <div className="notebook-battery">
+//         <img src="" alt="" />
+//         <p>{batteryDesc}</p>
+//       </div>
+
+//       <div className="notebook-facetime-camera">
+//         <img src="" alt="" />
+//         <p>720p Facetime HD camera</p>
+//       </div>
+
+//       <div className="notebook-weight">
+//         <h3>{weightTitle}</h3>
+//         <p>Weight</p>
+//       </div>
+
+//       <div className="notebook-touch-id">
+//         <img src="" alt="" />
+//         <p>{touchIdDesc}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NotebookSpecs;
