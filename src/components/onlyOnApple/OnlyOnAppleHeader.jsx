26148c764d22268244440f72e6d98f4e1677ff9b
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-13 17:31:31 -0400

    implemeted chapterbook nav bar in the only on apple component

--- src/components/onlyOnApple/OnlyOnAppleHeader.jsx
+++ src/components/onlyOnApple/OnlyOnAppleHeader.jsx
@@ -4,82 +4,82 @@ const OnlyOnAppleHeader = () => {
       <div className="mini_header_wrapper">
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/apple_tv_plus_light__bpnd29dq0aly_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple TV+</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/apple_music_light__dav7xuy6woom_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple Music</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/apple_fitness_light__cy5g4t5x3xci_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple Fitness+</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/apple_arcade_light__f21wrba92fma_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple Arcade</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/apple_news_light__eghyxu4b8342_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple News+</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/icloud_light__dyqbid0zktsi_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>iCloud</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/podcasts_light__euxq1obveieu_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple Podcasts</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/books_light__bttodo1o03yu_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple Books</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/apple_card_light__i3c6s5w8a82u_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Apple Card</p>
         </div>
 
         <div className="mini_header_items">
           <img
-            src="https://www.apple.com/v/airpods/r/images/chapternav/airpods_compare_light__cmuvdvbxj1w2_large.svg"
+            src="https://www.apple.com/v/services/e/images/services/chapternav/light/wallet_light__efe45y48pemq_large.svg"
             alt=""
           />
-          <p>Compare</p>
+          <p>Wallet</p>
         </div>
       </div>
     </div>
