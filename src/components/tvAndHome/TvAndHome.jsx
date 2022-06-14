530702f59a4d0eb6abcb4f13b23558bb70a680bd
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-14 00:23:51 -0400

    finished another portion of card design in onlyOnApple component

--- src/components/tvAndHome/TvAndHome.jsx
+++ src/components/tvAndHome/TvAndHome.jsx
@@ -29,59 +29,66 @@ const TvAndHome = () => {
           </p>
         </div>
 
-        <div className="tvAndHome_card_wrapper">
-          <div className="tvAndHome_card_left">
-            <h3 className="card_left_title">HomePod mini</h3>
-            <h1 className="card_left_desc">
-              Room-filling sound in every room.
-            </h1>
-            <p className="card_left_price">$99</p>
+        <div className="vAndHome_card_container">
+          <div className="tvAndHome_card_wrapper">
+            <div className="tvAndHome_card_left">
+              <h3 className="card_left_title">HomePod mini</h3>
+              <h1 className="card_left_desc">
+                Room-filling sound in every room.
+              </h1>
+              <p className="card_left_price">$99</p>
 
-            <div className="button_learn_more">
-              <button>Buy</button>
-              <span>Learn more {">"} </span>
+              <div className="button_learn_more">
+                <button>Buy</button>
+                <span>Learn more {">"} </span>
+              </div>
+              <img
+                src="https://www.apple.com/v/tv-home/d/images/overview/room_filling_sound__zagu3551kwyi_large.jpg"
+                alt=""
+              />
             </div>
-            <img
-              src="https://www.apple.com/v/tv-home/d/images/overview/room_filling_sound__zagu3551kwyi_large.jpg"
-              alt=""
-            />
+
+            <div className="tvAndHome_card_right">
+              <img
+                src="https://www.apple.com/v/tv-home/d/images/overview/apple_tv_4k_logo__vyjj7uki3tui_large.png"
+                alt=""
+              />
+              <h1 className="card_right_desc">
+                A higher definition of home entertainment.
+              </h1>
+              <p className="card_right_price">Starting $99</p>
+
+              <div className="button_learn_more">
+                <button>Buy</button>
+                <span>Learn more {">"} </span>
+              </div>
+              <img
+                src="https://www.apple.com/v/tv-home/d/images/overview/higher_definition__hfivribdc3ue_large.jpg"
+                alt=""
+              />
+            </div>
           </div>
 
-          <div className="tvAndHome_card_right">
-            <img
-              src="https://www.apple.com/v/tv-home/d/images/overview/apple_tv_4k_logo__vyjj7uki3tui_large.png"
-              alt=""
-            />
-            <h1 className="card_right_desc">
-              A higher definition of home entertainment.
-            </h1>
-            <p className="card_right_price">Starting $99</p>
+          <div className="tvAndHome_card_home_wrapper">
+            <div className="tvAndHome_card_home">
+              <div className="tvAndHome_card_home_left">
+                <img
+                  className="tvAndHome_card_home_img"
+                  src="https://www.apple.com/v/tv-home/d/images/overview/your_home_at_your_command__b1w2tomy2wia_large.jpg"
+                  alt=""
+                />
+              </div>
 
-            <div className="button_learn_more">
-              <button>Buy</button>
-              <span>Learn more {">"} </span>
+              <div className="tvAndHome_card_home_right">
+                <img
+                  src="https://www.apple.com/v/tv-home/d/images/overview/home_icon__9u57pp0qahea_large.png"
+                  alt=""
+                />
+                <p>Home</p>
+                <h1>Your home at your command.</h1>
+                <p className="learn_more">Learn more {">"}</p>
+              </div>
             </div>
-            <img
-              src="https://www.apple.com/v/tv-home/d/images/overview/higher_definition__hfivribdc3ue_large.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-
-        <div className="tvAndHome_card_home">
-          <img
-            src="https://www.apple.com/v/tv-home/d/images/overview/your_home_at_your_command__b1w2tomy2wia_large.jpg"
-            alt=""
-          />
-
-          <div className="tvAndHome_card_home_right">
-            <img
-              src="https://www.apple.com/v/tv-home/d/images/overview/home_icon__9u57pp0qahea_large.png"
-              alt=""
-            />
-            <p>Home</p>
-            <h1>Your home at your command.</h1>
-            <p>Learn more {">"}</p>
           </div>
         </div>
       </div>
