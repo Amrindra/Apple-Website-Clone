f9715a9684b895bcf1bcd43e7cd1b12ecfc7a406
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-17 19:09:03 -0400

    Passing props

--- src/components/accessories/Accessories.jsx
+++ src/components/accessories/Accessories.jsx
@@ -78,25 +78,90 @@ const Accessories = () => {
             <div className="accessories_card_feature_section_wrapper">
               <div className="accessories_card_feature_top">
                 <div className="feature_card1">
-                  <AccessoryCard className="feature_card_1" title="" />
+                  <AccessoryCard
+                    className="feature_card_1"
+                    img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MN6N3_FV532?wid=532&hei=582&fmt=png-alpha&.v=1651860910311"
+                    title="Pride Edition Nike Sport Loop"
+                    price="$49.00"
+                  />
                 </div>
                 <div className="feature_card2">
-                  <AccessoryCard className="feature_card_2" />
+                  <AccessoryCard
+                    className="feature_card_2"
+                    img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MN6L3_FV402?wid=1420&hei=930&fmt=png-alpha&.v=1651856288917"
+                    title="Pride Edition Sport Loop"
+                    price="$49.00"
+                  />
                 </div>
               </div>
             </div>
             <div className="accessories_card_feature_bottom">
-              <AccessoryCard className="feature_card_3" />
-              <AccessoryCard className="feature_card_4" />
-              <AccessoryCard className="feature_card_5" />
+              <AccessoryCard
+                className="feature_card_3"
+                img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MN653_AV3?wid=532&hei=582&fmt=png-alpha&.v=1645998988412"
+                title="iPhone 13 Pro Silicone Case With MagSage"
+                price="$49.00"
+              />
+              <AccessoryCard
+                className="feature_card_4"
+                img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MMMR3?wid=532&hei=582&fmt=png-alpha&.v=1645719947833"
+                title="Magic Keyboard with Touch ID and Numeric Keypad"
+                price="$199.00"
+              />
+              <AccessoryCard
+                className="feature_card_5"
+                img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MNA73_FV102?wid=532&hei=582&fmt=png-alpha&.v=1645214428592"
+                title="Smart Folio for iPad Air"
+                price="$79.00"
+              />
             </div>
 
+            {/* Wireless Headphones Section */}
             <div className="accessories_card_headphone_section">
               <h3>Wireless Headphones</h3>
               <div className="accessories_card_headphone">
-                <AccessoryCard className="feature_card_3" />
-                <AccessoryCard className="feature_card_4" />
-                <AccessoryCard className="feature_card_5" />
+                <AccessoryCard
+                  className="feature_card_3"
+                  img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=532&hei=582&fmt=png-alpha&.v=1632861342000"
+                  title="AirPods (3rd generation)"
+                  price="$179.00"
+                />
+                <AccessoryCard
+                  className="feature_card_4"
+                  img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWP22?wid=532&hei=582&fmt=png-alpha&.v=1591634795000"
+                  title="AirPods Pro"
+                  price="$249.00"
+                />
+                <AccessoryCard
+                  className="feature_card_5"
+                  img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/airpods-max-select-silver-202011?wid=532&hei=582&fmt=png-alpha&.v=1604021221000"
+                  title="AirPods Max - Silver"
+                  price="$549.00"
+                />
+              </div>
+            </div>
+
+            <div className="accessories_card_headphone_section">
+              <h3>AirTag</h3>
+              <div className="accessories_card_headphone">
+                <AccessoryCard
+                  className="feature_card_3"
+                  img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/airtag-single-select-202104?wid=532&hei=582&fmt=png-alpha&.v=1617761671000"
+                  title="AirTag"
+                  price=""
+                />
+                <AccessoryCard
+                  className="feature_card_4"
+                  img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/airtag-4pack-select-202104?wid=532&hei=582&fmt=png-alpha&.v=1617761669000"
+                  title="AirTag 4 pack"
+                  price=""
+                />
+                <AccessoryCard
+                  className="feature_card_5"
+                  img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MX4A2?wid=532&hei=582&fmt=png-alpha&.v=1618028917000"
+                  title="AirTag Leather Loop - Saddle Brown"
+                  price="$39.00"
+                />
               </div>
             </div>
           </div>
--- src/components/accessories/AccessoryCard.jsx
+++ src/components/accessories/AccessoryCard.jsx
@@ -5,16 +5,13 @@ const AccessoryCard = (props) => {
     <div className={`accessory_card ${props.className}`}>
       <div className="accessory_card_container">
         <div className="accessory_card_img">
-          <img
-            src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MN6N3_FV532?wid=532&hei=582&fmt=png-alpha&.v=1651860910311"
-            alt=""
-          />
+          <img src={props.img} alt="" />
         </div>
 
         <div className={`accessory_card_content `}>
           <p className="card_new">New</p>
-          <p className="card_title">Pride Edition Nike Sport Loop</p>
-          <p className="card_price">49.00$</p>
+          <p className="card_title">{props.title}</p>
+          <p className="card_price">{props.price}</p>
           <div className="card_color_options">
             <img
               src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MN683_SW_COLOR?wid=32&hei=32&fmt=png-alpha&.v=1645232602059"
