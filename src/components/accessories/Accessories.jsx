1c0425f036bc85fff20fd9d764a6e158b688617e
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-17 00:39:42 -0400

    Implemented onClick event listener for browse product and category

--- src/components/accessories/Accessories.jsx
+++ src/components/accessories/Accessories.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 
 const Accessories = () => {
   const [inputClick, setInputClick] = useState(true);
+  const [showBrowseProductCategory, setShowBrowseProductCategory] = useState(
+    true
+  );
   return (
     <div className={inputClick ? "accessories" : " accessories active"}>
       <div className="accessories_container">
@@ -40,141 +43,154 @@ const Accessories = () => {
 
         <div className="accessories_browse">
           <div className="accessories_browse_wrapper">
-            <button>Browser by Product</button>
-            <button>Browser by Category</button>
+            <button
+              onClick={() =>
+                setShowBrowseProductCategory(!showBrowseProductCategory)
+              }
+            >
+              Browser by Product
+            </button>
+            <button
+              onClick={() =>
+                setShowBrowseProductCategory(!showBrowseProductCategory)
+              }
+            >
+              Browser by Category
+            </button>
           </div>
         </div>
 
-        {/* Browse by product section */}
+        {/* Browse by product and category section */}
 
-        <div className="accessories_browser_by_product">
-          <div className="accessories_browser_by_product_wrapper">
-            <div className="browse_product_item">
-              <div className="browse_product_item_wrapper">
-                <div className="browse_product_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
-                    alt=""
-                  />
+        {showBrowseProductCategory ? (
+          <div className="accessories_browser_by_product">
+            <div className="accessories_browser_by_product_wrapper">
+              <div className="browse_product_item">
+                <div className="browse_product_item_wrapper">
+                  <div className="browse_product_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
+                      alt=""
+                    />
+                  </div>
                 </div>
+                <h3>Mac</h3>
               </div>
-              <h3>Mac</h3>
-            </div>
 
-            <div className="browse_product_item">
-              <div className="browse_product_item_wrapper">
-                <div className="browse_product_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-ipad?wid=150&hei=150&fmt=png-alpha&.v=1544482382704"
-                    alt=""
-                  />
+              <div className="browse_product_item">
+                <div className="browse_product_item_wrapper">
+                  <div className="browse_product_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-ipad?wid=150&hei=150&fmt=png-alpha&.v=1544482382704"
+                      alt=""
+                    />
+                  </div>
                 </div>
+                <h3>iPad</h3>
               </div>
-              <h3>iPad</h3>
-            </div>
 
-            <div className="browse_product_item">
-              <div className="browse_product_item_wrapper">
-                <div className="browse_product_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-iphone?wid=150&hei=150&fmt=png-alpha&.v=1544482382650"
-                    alt=""
-                  />
+              <div className="browse_product_item">
+                <div className="browse_product_item_wrapper">
+                  <div className="browse_product_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-iphone?wid=150&hei=150&fmt=png-alpha&.v=1544482382650"
+                      alt=""
+                    />
+                  </div>
                 </div>
+                <h3>iPhone</h3>
               </div>
-              <h3>iPhone</h3>
-            </div>
 
-            <div className="browse_product_item">
-              <div className="browse_product_item_wrapper">
-                <div className="browse_product_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-watch?wid=150&hei=150&fmt=png-alpha&.v=1544482383418"
-                    alt=""
-                  />
+              <div className="browse_product_item">
+                <div className="browse_product_item_wrapper">
+                  <div className="browse_product_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-watch?wid=150&hei=150&fmt=png-alpha&.v=1544482383418"
+                      alt=""
+                    />
+                  </div>
                 </div>
+                <h3>Watch</h3>
               </div>
-              <h3>Watch</h3>
-            </div>
 
-            <div className="browse_product_item">
-              <div className="browse_product_item_wrapper">
-                <div className="browse_product_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-tv?wid=150&hei=150&fmt=png-alpha&.v=1544482382741"
-                    alt=""
-                  />
+              <div className="browse_product_item">
+                <div className="browse_product_item_wrapper">
+                  <div className="browse_product_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-tv?wid=150&hei=150&fmt=png-alpha&.v=1544482382741"
+                      alt=""
+                    />
+                  </div>
                 </div>
+                <h3>TV & Home</h3>
               </div>
-              <h3>TV & Home</h3>
-            </div>
-          </div>
-        </div>
-
-        {/* Browse by category section */}
-        <div className="accessories_browser_by_category">
-          <div className="accessories_browser_by_category_wrapper">
-            <div className="browse_category_item">
-              <div className="browse_category_item_wrapper">
-                <div className="browse_category_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <h3>Mac</h3>
-            </div>
-
-            <div className="browse_category_item">
-              <div className="browse_category_item_wrapper">
-                <div className="browse_category_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <h3>Mac</h3>
-            </div>
-
-            <div className="browse_category_item">
-              <div className="browse_category_item_wrapper">
-                <div className="browse_category_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <h3>Mac</h3>
-            </div>
-
-            <div className="browse_category_item">
-              <div className="browse_category_item_wrapper">
-                <div className="browse_category_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <h3>Mac</h3>
-            </div>
-
-            <div className="browse_category_item">
-              <div className="browse_category_item_wrapper">
-                <div className="browse_category_img_wrapper">
-                  <img
-                    src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <h3>Mac</h3>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="accessories_browser_by_category">
+            <div className="accessories_browser_by_category_wrapper">
+              <div className="browse_category_item">
+                <div className="browse_category_item_wrapper">
+                  <div className="browse_category_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <h3>Mac</h3>
+              </div>
+
+              <div className="browse_category_item">
+                <div className="browse_category_item_wrapper">
+                  <div className="browse_category_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <h3>Mac</h3>
+              </div>
+
+              <div className="browse_category_item">
+                <div className="browse_category_item_wrapper">
+                  <div className="browse_category_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <h3>Mac</h3>
+              </div>
+
+              <div className="browse_category_item">
+                <div className="browse_category_item_wrapper">
+                  <div className="browse_category_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <h3>Mac</h3>
+              </div>
+
+              <div className="browse_category_item">
+                <div className="browse_category_item_wrapper">
+                  <div className="browse_category_img_wrapper">
+                    <img
+                      src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/icon-product-mac?wid=150&hei=150&fmt=png-alpha&.v=1544482382615"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <h3>Mac</h3>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
