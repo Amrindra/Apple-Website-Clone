5ecf7690870b0061426c565413b0a12b6df24d67
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-19 19:39:35 -0400

    Worked on 2nd banner it's half way done

--- src/components/airPod/AirPod.jsx
+++ src/components/airPod/AirPod.jsx
@@ -1,35 +1,26 @@
 import "./airpod.css";
-import AirPodHeader from "./AirPodsHeader";
 
-const AirPod = () => {
+const AirPod = (props) => {
   return (
     <div className="airPod_container">
-      <AirPodHeader />
-
-      <div className="airPod_hero_banner">
+      <div className={`airPod_hero_banner ${props.className}`}>
         {/* <video autoplay loop muted plays-inline>
           <source
             src="https://www.apple.com/105/media/us/airpods/2022/7052f60e-c8a4-4a32-b278-0d0348fbcd0a/anim/spatial/large_2x.mp4"
             type="video/mp4"
           />
         </video> */}
-        <div className="airPod_banner_img">
+        <div className={`airPod_banner_img ${props.className}`}>
           <div className="airPod_banner_img_wrapper">
-            <img
-              src="https://www.apple.com/v/airpods/r/images/overview/airpods_pro_left__bvanpj0ujnf6_medium_2x.png"
-              alt=""
-            />
-            <img
-              src="https://www.apple.com/v/airpods/r/images/overview/airpods_pro_right__fm0wwisa76em_large_2x.png"
-              alt=""
-            />
+            <img src={props.img1} alt="" className="airPod_img_1" />
+            <img src={props.img2} alt="" className="airPod_img_2" />
           </div>
 
           <div className="airPod_banner_content">
             <div className="airPod_banner_content_wrapper">
               <div className="airPod_banner_content_top">
                 <h1>
-                  AirPods <span>Pro</span>
+                  {props.modelTitle} <span>{props.pro}</span>
                 </h1>
                 <p>$269</p>
               </div>
--- src/pages/AirPodPage.jsx
+++ src/pages/AirPodPage.jsx
@@ -1,9 +1,24 @@
 import AirPod from "../components/airPod/AirPod";
+import AirPodHeader from "../components/airPod/AirPodsHeader";
 
 const AirPodPage = () => {
   return (
     <div>
-      <AirPod />
+      <AirPodHeader />
+      <AirPod
+        img1="https://www.apple.com/v/airpods/r/images/overview/airpods_pro_left__bvanpj0ujnf6_medium_2x.png"
+        img2="https://www.apple.com/v/airpods/r/images/overview/airpods_pro_right__fm0wwisa76em_large_2x.png"
+        modelTitle="AirPods"
+        pro="Pro"
+        price="$299"
+        className="airPod_pro"
+      />
+      <AirPod
+        img1="https://www.apple.com/v/airpods/r/images/overview/airpods_max__f265q4g4ddym_large_2x.png"
+        img2="https://www.apple.com/v/airpods/r/images/overview/airpods_max_logo__bdgdixidcqcy_large_2x.jpg"
+        price="$299"
+        className="airPod_max"
+      />
     </div>
   );
 };
