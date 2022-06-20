2f3342f92006b7dfb1a89529606c1c4559ab2c18
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-20 18:40:56 -0400

    create watch scss and implemented first banner section for watch page

--- src/components/watch/Watch.jsx
+++ src/components/watch/Watch.jsx
@@ -1,39 +1,56 @@
-import "../../styles/watch/Watch.css";
-import HeaderBook from "../headerBook/HeaderBook";
+import "./Watch.scss";
 
 const Watch = () => {
   return (
     <div className="watch">
-      <HeaderBook
-        img1="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_nike_light__b7zt65jh0i76_large.svg"
-        img2="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_se_light__c2mzuz6w15km_large.svg"
-        img3="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_series_3_light__mmwjed73owia_large.svg"
-        img4="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_nike_light__b7zt65jh0i76_large.svg"
-        img5="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_hermes_light__danq731vuiy6_large.svg"
-        img6="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_studio_light__btlmt8t0oe02_large.svg"
-        img7="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_compare__b9nnoiqjkxki_large.svg"
-        img8="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_bands__fyke1oo6sfyy_large.svg"
-        img9="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_airpods__b481yj4nw0vm_large.svg"
-        img10="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_accessories__bu98chbn5rea_large.svg"
-        img11="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_fitness__gb78ob6kpiem_large.svg"
-        img12="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_watch_os__bv24twycj3aq_large.svg"
-        img13="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_shop_watch_light__fkrdcz8nfwi2_large.svg"
-        brandTitle="Apple Watch"
-        model1="Series 7"
-        model2="SE"
-        model3="Series 3"
-        model4="Nike"
-        model5="Hermes"
-        model6="Studio"
-        compare="Compare"
-        bands="Bands"
-        airPods="AirPods"
-        accessories="Accessories"
-        appleFitness="Apple Fitness+"
-        watch="watchOS"
-        shopWatch="Shop Watch"
-        className="watch_section"
-      />
+      <div className="watch_container">
+        <div className="watch_content_1_wrapper">
+          <img
+            className="title_img"
+            src="https://www.apple.com/v/watch/ax/images/overview/series-7/logo_watch_s7__dnxp9zoaom82_medium_2x.png"
+            alt=""
+          />
+          <h3>Full screen ahead.</h3>
+          <p>From $399</p>
+
+          <div className="watch_content_links">
+            <button>Buy</button>
+            <div className="watch_content_link_item">
+              <span>Learn more</span>
+              <span>Watch Film</span>
+            </div>
+          </div>
+
+          <img
+            src="https://www.apple.com/v/watch/ax/images/overview/series-7/hero_s7__ep2maoos292e_large.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="watch_display">
+        <div className="watch_display_container">
+          {/* <div className="watch_display_content_2_wrapper"> */}
+          <div className="watch_content_2_left">
+            <img
+              className="content_2_title_img"
+              src="https://www.apple.com/v/watch/ax/images/overview/display/display_s7__fj2bqp6brfiy_large.jpg"
+              alt=""
+            />
+          </div>
+
+          <div className="watch_content_2_right">
+            <p>Our largest display yet.</p>
+            <h3>Over 50% more screen area than Series 3.</h3>
+
+            <div className="watch_content_toggle_buttons">
+              <button>Series 7</button>
+              <button>Series 3</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
     </div>
   );
 };
--- src/pages/WatchPage.jsx
+++ src/pages/WatchPage.jsx
@@ -1,8 +1,39 @@
 import Watch from "../components/watch/Watch";
+import HeaderBook from "../components/headerBook/HeaderBook";
 
 const WatchPage = () => {
   return (
     <div>
+      <HeaderBook
+        img1="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_nike_light__b7zt65jh0i76_large.svg"
+        img2="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_se_light__c2mzuz6w15km_large.svg"
+        img3="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_series_3_light__mmwjed73owia_large.svg"
+        img4="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_nike_light__b7zt65jh0i76_large.svg"
+        img5="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_hermes_light__danq731vuiy6_large.svg"
+        img6="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_studio_light__btlmt8t0oe02_large.svg"
+        img7="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_compare__b9nnoiqjkxki_large.svg"
+        img8="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_bands__fyke1oo6sfyy_large.svg"
+        img9="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_airpods__b481yj4nw0vm_large.svg"
+        img10="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_accessories__bu98chbn5rea_large.svg"
+        img11="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_fitness__gb78ob6kpiem_large.svg"
+        img12="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_watch_os__bv24twycj3aq_large.svg"
+        img13="https://www.apple.com/v/watch/aw/images/overview/icons/watch_nav_shop_watch_light__fkrdcz8nfwi2_large.svg"
+        brandTitle="Apple Watch"
+        model1="Series 7"
+        model2="SE"
+        model3="Series 3"
+        model4="Nike"
+        model5="Hermes"
+        model6="Studio"
+        compare="Compare"
+        bands="Bands"
+        airPods="AirPods"
+        accessories="Accessories"
+        appleFitness="Apple Fitness+"
+        watch="watchOS"
+        shopWatch="Shop Watch"
+        className="watch_section"
+      />
       <Watch />
     </div>
   );
