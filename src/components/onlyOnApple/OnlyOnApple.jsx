bb198a2b4e426793f46e3d0ff3e94ea1a837ff8e
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-22 16:53:06 -0400

    created intro for OnlyonApple component

--- src/components/onlyOnApple/OnlyOnApple.jsx
+++ src/components/onlyOnApple/OnlyOnApple.jsx
@@ -1,10 +1,24 @@
-import HeaderBook from "../headerBook/HeaderBook";
+import "./OnlyOnApple.scss";
 import OnlyOnAppleHeader from "./OnlyOnAppleHeader";
 
 const OnlyOnApple = () => {
   return (
     <div className="onlyOnApple">
       <OnlyOnAppleHeader />
+
+      <div className="only_apple_intro">
+        <div className="only_apple_intro_wrapper">
+          <h1>The best experiences.</h1>
+          <h1>Only on Apple.</h1>
+          <p>
+            Get the most out of the devices you love with high‑quality content
+            and services. Award‑winning series and films, amazing music in
+            spatial audio, world-class workouts and meditations, trusted news
+            publications, superfun games — even the ways you pay for things. And
+            they’re only on Apple.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
